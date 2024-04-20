c8708fa0f26e9982f14413760fb0aadffe2b7966
majbaulhoque <majba9399@gmail.com>
2024-04-20 22:49:16 +0600

    truly darkmode on

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,14 @@ const Navbar = ({ className }: { className?: string }) => {
         className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
         >
             <Menu setActive={setActive}>
-                <Link href={"#"}>
+                <Link href={"/"}>
                 <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
                 </Link>
+                <MenuItem setActive={setActive} active={active} item="Our Courses">
+                <div className="flex flex-col space-y-4 text-sm">
+                    <HoveredLink href="/courses">All Courses</HoveredLink>
+                </div>
+                </MenuItem>
             </Menu>
         </div>
     )
