85d5f5f2ec109880c2d150fc8ec7a26439d67358
majbaulhoque <majba9399@gmail.com>
2024-04-20 23:02:32 +0600

    basic structure nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,8 +17,15 @@ const Navbar = ({ className }: { className?: string }) => {
                 <MenuItem setActive={setActive} active={active} item="Our Courses">
                 <div className="flex flex-col space-y-4 text-sm">
                     <HoveredLink href="/courses">All Courses</HoveredLink>
+                    <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+                    <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+                    <HoveredLink href="/courses">Songwriting</HoveredLink>
+                    <HoveredLink href="/courses">Music Production</HoveredLink>
                 </div>
                 </MenuItem>
+                <Link href={"/contact"}>
+                    <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
+                </Link>
             </Menu>
         </div>
     )
