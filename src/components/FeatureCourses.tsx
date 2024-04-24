ced04f923019a93adc830353ebac060ad17804eb
majbaulhoque <majba9399@gmail.com>
2024-04-24 22:50:31 +0600

    musical card added and responsive

--- src/components/FeatureCourses.tsx
+++ src/components/FeatureCourses.tsx
@@ -1,10 +1,26 @@
+"use client"
+import React from 'react';
 import courseData from "../data/music_courses.json"
 import Link from "next/link"
 import { Button } from "./ui/moving-border";
+// import { BackgroundGradient } from "./ui/background-gradient";
+import { BackgroundGradient } from "./ui/background-gradient";
 
+
+interface Course{
+        id: number,
+        title: string,
+        slug: string,
+        description: ".",
+        price: number,
+        instructor: string,
+        isFeatured: boolean,
+        // image: string 
+}
 
 function FeatureCourses() {
-    courseData.courses.filter(course => course.isFeatured)
+    // const featureCourses = courseData.courses.filter((course:Courses) => course.isFeatured)
+    const featuredCourses = (courseData.courses as Course[]).filter((course) => course.isFeatured);
     return (
         <div className="py-12 bg-gray-900">
             <div>
@@ -16,7 +32,27 @@ function FeatureCourses() {
                 </div>
                 <div className="mt-10">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
-
+                        {
+                            featuredCourses.map((course:
+                                Course) => (
+                                <div key={course.id} className="flex justify-center">
+                                <BackgroundGradient className='flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm'>
+                                <div 
+                                className='p-4 sm:p-6 flex flex-col items-center text-center flex-grow'>
+                                    <p
+                                    className='text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200'
+                                    >{course.title}</p>
+                                    <p className='text-sm text-neutral-600 dark:text-neutral-400 flex-grow'>{course.description}</p>
+                                    <Link href={`/courses/${course.slug}`}>
+                                    Learn More
+                                    </Link>
+                                </div>
+                                </BackgroundGradient>
+                                        
+                                    
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
                 <div className="mt-20 text-center">
