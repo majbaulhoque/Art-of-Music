be3a2932cabce16e43a02f6c8fd410e39f32196c
majbaulhoque <majba9399@gmail.com>
2024-04-25 09:46:17 +0600

    add infinite moving cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeatureCourses from "@/components/FeatureCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonial from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection></HeroSection>
       <FeatureCourses></FeatureCourses>
       <WhyChooseUs></WhyChooseUs>
+      <MusicSchoolTestimonial></MusicSchoolTestimonial>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,8 @@ const config: Config = {
   theme: {
     extend: {
       animation: {
+        scroll: 
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       backgroundImage: {
@@ -34,6 +36,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
         spotlight: {
           "0%": {
             opacity: '0',
